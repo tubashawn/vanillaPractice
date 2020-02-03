fcdf8d219ea9146109da2d2f1fddf64d88d02e84
tubaShawn <shawnrmangum@gmail.com>
2020-02-03 16:51:22 -0700

    cleaned up console.logs, added basic timing calculation

--- Assets/interactive.js
+++ Assets/interactive.js
@@ -3,33 +3,64 @@ const HEADER = document.getElementById("header");
 const BUTTON = document.getElementById("button");
 const USERNUMBER = document.getElementById("userNumber");
 const USERNUMBERS = document.getElementById("userNumbers");
-const SORT = document.getElementById("sortButton");
+const SORTBUTTON = document.getElementById("sortButton");
 const ORDEREDNUMBERS = document.getElementById("orderedNumbers");
 const SECTION5INFO = document.getElementById("section5info");
 
 let userArray = [];
+let startTime, endTime;
 
-SECTION5INFO.addEventListener("click", function(){
-    logger(userArray);
-});
 
-SORT.addEventListener("click", function(){
-    sorter(userArray);
-});
+
 
 BUTTON.addEventListener("click", function() {
     numberLogger(userArray);
 });
 
+SORTBUTTON.addEventListener("click", function(){
+    bubbleSorter(userArray);
+});
 
 
 function logger() {
     console.log("Random number = " + randomizer());
 }
 
+function start() {
+    startTime = new Date();
+}
+
+function end() {
+    endTime = new Date();
+}
 
 function randomizer() {
     return Math.floor(Math.random() * 100);
+}
+
+function randomNumbers(number) {
+    let numbers = [];
+    for (let i = 0; i < number; i++) {
+        let temp = randomizer();
+        numbers.push(temp);
+    }
+    userArray = numbers;
+    console.log(userArray);
+}
+
+function bubbleSorter(array) {
+    start();
+    for (let i = 0 ; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] > array[j+1]) {
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    end();
+    console.log(`${endTime-startTime} milliseconds passed`);
 }
 
 function numberLogger() {
@@ -42,29 +73,4 @@ function numberLogger() {
 }
 
 
-function randomNumbers(number) {
-    let numbers = [];
-    for (let i = 0; i < number; i++) {
-        let temp = randomizer();
-        numbers.push(temp);
-    }
-    userArray = numbers;
-    console.log(userArray);
-}
 
-function sorter(array) {
-    for (let i = 0 ; i < array.length; i++) {
-        console.log(`${array[i]} and ${array[i+1]}`);
-        for (let j = 0; j < array.length; j++) {
-
-            if (array[j] > array[j+1]) {
-                let temp = array[j];
-                console.log(temp);
-                array[j] = array[j+1];
-                array[j+1] = temp;
-            }
-        }
-        
-    }
-    console.log(array);
-}
