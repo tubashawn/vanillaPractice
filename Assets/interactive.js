32bed927bb72ac0d59018e09d4b399bdef17b543
tubaShawn <shawnrmangum@gmail.com>
2020-02-03 15:21:35 -0700

    fixed broken stuff, moving on

--- Assets/interactive.js
+++ Assets/interactive.js
@@ -9,26 +9,38 @@ const SECTION5INFO = document.getElementById("section5info");
 
 let userArray = [];
 
+SECTION5INFO.addEventListener("click", function(){
+    logger(userArray);
+});
 
-BUTTON.addEventListener("click", numberLogger(userArray));
-SECTION5INFO.addEventListener("click", sorter(userArray));
+SORT.addEventListener("click", function(){
+    sorter(userArray);
+});
+
+BUTTON.addEventListener("click", function() {
+    numberLogger(userArray);
+});
+
+
 
 function logger() {
     console.log("Random number = " + randomizer());
 }
 
 
-function numberLogger(array) {
-    let numberValue = document.getElementById("questionInput").value;
-    
-    USERNUMBER.innerHTML = `You entered ${numberValue}`;
-    console.log(userArray);
-    USERNUMBERS.innerHTML = userArray;
-}
-
 function randomizer() {
     return Math.floor(Math.random() * 100);
 }
+
+function numberLogger() {
+    let numberValue = document.getElementById("questionInput").value;
+    randomNumbers(numberValue);
+    
+    USERNUMBER.innerHTML = `You entered ${numberValue}`;
+    USERNUMBERS.innerHTML = userArray;
+    
+}
+
 
 function randomNumbers(number) {
     let numbers = [];
@@ -41,10 +53,14 @@ function randomNumbers(number) {
 }
 
 function sorter(array) {
-    console.log(`Inside the sorter function using ${array}`);
     for (let i = 0 ; i < array.length; i++) {
+        console.log(`${array[i]} and ${array[i+1]}`);
+        
+        let temp;
         if (array[i] > array[i+1]) {
-            let temp = array[i];
+            console.log("in if statement");
+            temp = array[i];
+            console.log(temp);
             array[i] = array[i+1];
             array[i+1] = temp;
         }
