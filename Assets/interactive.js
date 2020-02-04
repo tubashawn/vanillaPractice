092a84c1c2496c83a46f3f72c1af2cbe83c19bd8
tubaShawn <shawnrmangum@gmail.com>
2020-02-03 21:06:33 -0700

    added new button, cleaned up js file

--- Assets/interactive.js
+++ Assets/interactive.js
@@ -1,9 +1,10 @@
 const QUESTION = document.getElementById("questionInput");
 const HEADER = document.getElementById("header");
 const BUTTON = document.getElementById("button");
+const BUBBLESORTBUTTON = document.getElementById("bubbleSortButton");
+const SELECTIONSORTBUTTON = document.getElementById("selectionSortButton");
 const USERNUMBER = document.getElementById("userNumber");
 const USERNUMBERS = document.getElementById("userNumbers");
-const SORTBUTTON = document.getElementById("sortButton");
 const ORDEREDNUMBERS = document.getElementById("orderedNumbers");
 const SECTION5INFO = document.getElementById("section5info");
 
@@ -60,7 +61,7 @@ const SORTS = {
                 } else {}
             }
             if (min !== i) {
-                
+                this.swap(array, i, min);
             } else {}
         }
         ACTIONS.end();
@@ -73,7 +74,11 @@ BUTTON.onclick =  function() {
     numberLogger(userArray);
 }
 
-SORTBUTTON.onclick =  function(){
+BUBBLESORTBUTTON.onclick =  function(){
+    ORDEREDNUMBERS.innerHTML = SORTS.bubbleSorter(userArray);
+};
+
+SELECTIONSORTBUTTON.onclick =  function(){
     ORDEREDNUMBERS.innerHTML = SORTS.selectionSorter(userArray);
 };
 
@@ -84,45 +89,6 @@ function randomNumbers(number) {
         numbers.push(temp);
     }
     userArray = numbers;
-}
-
-// function bubbleSorter(array) {
-//     ACTIONS.start();
-//     for (let i = 0 ; i < array.length; i++) {
-//         for (let j = 0; j < array.length; j++) {
-//             if (array[j] > array[j+1]) {
-//                 let temp = array[j];
-//                 array[j] = array[j+1];
-//                 array[j+1] = temp;
-//             }
-//         }
-//     }
-//     ACTIONS.end();
-//     console.log(`${ACTIONS.elapsed()} milliseconds passed`);
-//     return array;
-// }
-
-function selectionSorter(array) {
-    let length = array.length;
-
-    for (let i = 0; i < length - 1; i++) {
-        var min = i;
-        for (let j = i + 1; j < length; j++) {
-            if (array[j] < array[min]) {
-                min = j;
-            } else {}
-        }
-        if (min !== i) {
-            swap(array, i, min);
-        } else {}
-    }
-    return array;
-}
-
-function swap(array, x, y) {
-    var temp = array[x];
-    array[x] = array[y];
-    array[y] = temp;
 }
 
 function numberLogger() {
